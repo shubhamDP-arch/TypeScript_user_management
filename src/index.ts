1b0c510d8e912855f2e2be95fe569701c099f305
backend <shubhamprabhu1001@gmail.com>
2024-11-23 23:21:15 +0530

    Setup complete

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import 'module-alias/register';
+
+import App from './app';
+import validateEnv from './utils/validateEnv';
+// import PostController from '@/resources/post/post.controller';
+// import UserController from '@/resources/user/user.controller';
+
+validateEnv()
+
+const app = new App([],
+    Number(process.env.PORT)
+);
+
+app.listen();
